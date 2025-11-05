c28ac59c861e4f3ed67f2410d04ccce8915c6928
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-11-05 15:43:58 -0800

    possible moves list added

--- KnightsTravails.js
+++ KnightsTravails.js
@@ -8,3 +8,28 @@ const chessBoard = [
   [0, 1, 2, 3, 4, 5, 6, 7],
   [0, 1, 2, 3, 4, 5, 6, 7],
 ];
+
+// All the possible moves for the knight from a point
+// Important: Assumes all moves stay on the chessboard.
+
+const possibleMoves = [
+  [-1, 2],
+  [1, 2],
+  [2, 1],
+  [2, -1],
+  [1, -2],
+  [-1, -2],
+  [-2, -1],
+  [-2, 1],
+];
+
+const calculateAllMoves = (position) => {
+  let moves = [];
+};
+
+// Is move on chessboard?
+// If not, remove from calculated moves.
+const isMoveValid = () => {};
+
+// TESTS
+console.log(calculateAllMoves([3, 4]));
