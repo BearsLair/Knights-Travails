e76265071ad9691e746369f0802f0a5a6eeacdf4
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-11-05 16:15:23 -0800

    calculated moves stays on the chessboard now

--- KnightsTravails.js
+++ KnightsTravails.js
@@ -24,12 +24,30 @@ const possibleMoves = [
 ];
 
 const calculateAllMoves = (position) => {
+  let x = 0;
+  let y = 0;
+  let endLocation = [];
   let moves = [];
+
+  for (let i = 0; i < 8; i++) {
+    x = possibleMoves[i][0] + position[0];
+    if (x < 0 || x > 7) {
+      x = null;
+    }
+    y = possibleMoves[i][1] + position[1];
+    if (y < 0 || y > 7) {
+      y = null;
+    }
+
+    if (x !== null && y !== null) {
+      endLocation = [x, y];
+
+      moves.push(endLocation);
+    }
+  }
+
+  return moves;
 };
 
-// Is move on chessboard?
-// If not, remove from calculated moves.
-const isMoveValid = () => {};
-
 // TESTS
-console.log(calculateAllMoves([3, 4]));
+console.log(calculateAllMoves([6, 6]));
