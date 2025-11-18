331dc1af6bc7b3b883b73210864f16a3cbff7991
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-11-17 22:58:38 -0800

    Can confirm single move. yay!

--- KnightsTravails.js
+++ KnightsTravails.js
@@ -1,18 +1,7 @@
-const chessBoard = [
-  [0, 1, 2, 3, 4, 5, 6, 7],
-  [0, 1, 2, 3, 4, 5, 6, 7],
-  [0, 1, 2, 3, 4, 5, 6, 7],
-  [0, 1, 2, 3, 4, 5, 6, 7],
-  [0, 1, 2, 3, 4, 5, 6, 7],
-  [0, 1, 2, 3, 4, 5, 6, 7],
-  [0, 1, 2, 3, 4, 5, 6, 7],
-  [0, 1, 2, 3, 4, 5, 6, 7],
-];
-
-// All the possible moves for the knight from a point
+// All the possible movements, using a cartesian coordinate system, for the knight from its current position.
 // Important: Assumes all moves stay on the chessboard.
 
-const possibleMoves = [
+const possibleMovements = [
   [-1, 2],
   [1, 2],
   [2, 1],
@@ -23,7 +12,9 @@ const possibleMoves = [
   [-2, 1],
 ];
 
-// Calculates and stores all moves that stay on chessboard
+// Calculates all moves (x, y coordinates) that stay on chessboard from knight's current position,
+// and returns array with the valid moves
+
 const calculateAllMoves = (position) => {
   let x = 0;
   let y = 0;
@@ -31,11 +22,11 @@ const calculateAllMoves = (position) => {
   let moves = [];
 
   for (let i = 0; i < 8; i++) {
-    x = possibleMoves[i][0] + position[0];
+    x = possibleMovements[i][0] + position[0];
     if (x < 0 || x > 7) {
       x = null;
     }
-    y = possibleMoves[i][1] + position[1];
+    y = possibleMovements[i][1] + position[1];
     if (y < 0 || y > 7) {
       y = null;
     }
@@ -50,5 +41,49 @@ const calculateAllMoves = (position) => {
   return moves;
 };
 
+// Calculate all possible moves that would lead to end position in one move.
+// The knight must eventually reach one of these positions, if it's not on one
+// already.
+
+const calculateEndMoves = (endPosition) => {
+  return calculateAllMoves(endPosition);
+};
+
+const knightMoves = (startPosition, endPosition) => {
+  const lastMoves = calculateEndMoves(endPosition);
+  let movesList = [];
+  let resultMessage = "";
+
+  // Checks if startPosition is in the array of lastMoves
+  const containsStartPosition = lastMoves.some(
+    (innerArray) =>
+      innerArray.length === startPosition.length &&
+      innerArray.every((value, index) => value === startPosition[index])
+  );
+
+  if (containsStartPosition) {
+    movesList = [startPosition, endPosition];
+
+    console.log("movesList", movesList);
+
+    resultMessage = `You made it in 1 move! Here's your path:\n${movesList[0]}\n${movesList[1]}`;
+
+    return resultMessage;
+  }
+
+  console.log("lastMoves: ", lastMoves);
+
+  // const startMoves = calculateAllMoves(startPosition);
+
+  console.log("startPosition: ", startPosition);
+
+  console.log(
+    "Is starting positions in last move array?: ",
+    containsTargetArray(startPosition, lastMoves)
+  );
+
+  return "huh?";
+};
+
 // TESTS
-console.log(calculateAllMoves([3, 7]));
+console.log(knightMoves([3, 3], [1, 2]));
